8d68259f3fae0703ff72d41dfb88dd2a681fd736
nz <niuzhe.nz@gmail.com>
2017-01-12 13:14:09 +0800

    modify ploting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
 from nxgboost import NXGBoost
 from sklearn.ensemble import RandomForestRegressor
 
@@ -35,11 +36,11 @@ def load_data(name):
 
 def nxgb_callback(train_x, train_y, test_x):
     nxgb = NXGBoost()
-    nxgb.fit(train_x, train_y, n_estimators=30, eta=1e-1, lambd=1e-3, max_depth=10)
+    nxgb.fit(train_x, train_y, n_estimators=50, eta=15e-2, lambd=13e-2, max_depth=10)
     return nxgb.predict(test_x)
 
 def rf_callback(train_x, train_y, test_x):
-    rf = RandomForestRegressor(n_estimators=30, max_depth=10)
+    rf = RandomForestRegressor(n_estimators=50, max_depth=10)
     rf.fit(train_x, train_y.ravel())
     return rf.predict(test_x)
 
@@ -48,30 +49,45 @@ def run_model(callback, train_x, train_y, test_x, test_y):
     for i in xrange(int(train_y.max()) + 1):
         test_y_.append(callback(train_x, (train_y == i).astype(np.float32), test_x).reshape([-1, 1]))
     test_y_ = np.argmax(test_y_, axis=0)
-    print test_y_.ravel()
     return np.mean(np.equal(test_y_, test_y).astype(np.float32))
 
 def run(name):
     data = load_data(name)
     nxgb_acc = run_model(nxgb_callback, *data)
     rf_acc = run_model(rf_callback, *data)
-
-    fig, ax = plt.subplots()
-    sns.barplot(['nxgboost', 'random forest'], [nxgb_acc, rf_acc], palette='Set3', ax=ax)
-    ax.set_ylabel('accuracy')
-    fig.savefig('fig/' + name + '.png')
-    plt.title('dataset ' + name)
-    # plt.show()
+    return nxgb_acc, rf_acc
 
 if __name__ == '__main__':
-    np.random.seed(5)
+    np.random.seed(27)
     dataset_names = set()
+    result = []
     for file in os.listdir('data'):
         filename = file.split('.txt')[0]
         dataset_names.add(filename.split('label')[0])
     i = 0
     for dataset_name in dataset_names:
         i += 1
-        print '[' + str(i) + '/' + str(len(dataset_names)) + ']calculating dataset', dataset_name
-        print '...'
-        run(dataset_name)
+        print '[' + str(i) + '/' + str(len(dataset_names)) + '] Calculating dataset', dataset_name, '...'
+        
+        nxgb_acc, rf_acc = run(dataset_name)
+        print 'nxgboost acc:\t', nxgb_acc
+        print 'random foreset acc:\t', rf_acc
+        print
+
+        result.append(('nxgboost', dataset_name, nxgb_acc))
+        result.append(('random forest', dataset_name, rf_acc))
+
+    print 'Calculation Finished.'
+
+    result = pd.DataFrame(result, columns=['model', 'dataset', 'accuracy'])
+
+    sns.set(style="whitegrid")
+    g = sns.factorplot("dataset", "accuracy", "model", 
+                    data=result, saturation=5, 
+                    size=4, aspect=3, 
+                    kind="bar", legend=False)
+    plt.legend(loc='upper right')
+    plt.savefig('fig/result.png')
+    print 'Result is saved into the /fig folder'
+
+    plt.show()
